c02bf0b9ba9f6a07e11d35eaad05aff6a9360b40
OlhaLuhanska <olyaelk@gmail.com>
2023-05-25 20:51:08 +0300

    bootstrap7

--- src/route/index.js
+++ src/route/index.js
@@ -1993,6 +1993,16 @@ router.get('/bootstrap5', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
+router.get('/bootstrap7', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap7', {
+    layout: 'default',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 // ================================================================
 
 // Підключаємо роутер до бек-енду
